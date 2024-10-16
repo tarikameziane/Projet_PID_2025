ee232fa4e43f9807c01be5e7e2e74743f9b8e86f
Tarik <ameziane.tarik@gmail.com>
2024-10-16 20:06:33 +0200

    first commit

---
+++ src/main/java/com/example/projet_pid_2025/ProjetPid2025Application.java
@@ -0,0 +1,13 @@
+package com.example.projet_pid_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetPid2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetPid2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projet_pid_2025/ProjetPid2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet_pid_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetPid2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
