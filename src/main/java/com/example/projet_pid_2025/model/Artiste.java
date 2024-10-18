d8c91a938bec5a50e4bef5ec17d424bbd46b252e
Tarik <ameziane.tarik@gmail.com>
2024-10-18 16:58:43 +0200

    Modefication

--- src/main/java/com/example/projet_pid_2025/model/Artiste.java
+++ src/main/java/com/example/projet_pid_2025/model/Artiste.java
@@ -1,2 +1,21 @@
-package com.example.projet_pid_2025.model;public class Artiste {
+package com.example.projet_pid_2025.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+
+@Entity
+@Data
+@Table(name="artistes")
+public class Artiste {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private String Nom ;
+    private String Prenom ;
+    private Date DateDeNaissance ;
 }
