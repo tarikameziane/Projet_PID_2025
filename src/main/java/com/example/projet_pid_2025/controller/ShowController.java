1d978c68420485179b84f8d7e685b25e46f16000
Tarik <ameziane.tarik@gmail.com>
2025-03-18 13:08:24 +0100

    Update templates Show (added artists’ list)"

--- src/main/java/com/example/projet_pid_2025/controller/ShowController.java
+++ src/main/java/com/example/projet_pid_2025/controller/ShowController.java
@@ -1,5 +1,7 @@
 package com.example.projet_pid_2025.controller;
 
+import com.example.projet_pid_2025.model.ArtistType;
+import com.example.projet_pid_2025.model.Artiste;
 import com.example.projet_pid_2025.model.Show;
 import com.example.projet_pid_2025.services.ShowService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +10,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 @Controller
 public class ShowController {
     @Autowired
@@ -28,11 +34,26 @@ public class ShowController {
     public String show(Model model, @PathVariable("id") String id) {
         Show show = service.get(id);
 
+        //Récupérer les artistes du spectacle et les grouper par type
+        Map<String, ArrayList<Artiste>> collaborateurs = new TreeMap<>();
+
+        for(ArtistType at : show.getArtistTypes()) {
+            String type = at.getType().getType();
+
+            if(collaborateurs.get(type) == null) {
+                collaborateurs.put(type, new ArrayList<>());
+            }
+
+            collaborateurs.get(type).add(at.getArtist());
+        }
+
+        model.addAttribute("collaborateurs", collaborateurs);
         model.addAttribute("show", show);
         model.addAttribute("title", "Fiche d'un spectacle");
 
         return "show/show";
     }
+
 
 }
 
