81c4eea35183d15fb0181c0d0adf80e24a50fb6f
Tarik <ameziane.tarik@gmail.com>
2025-03-18 13:17:20 +0100

    Relation ManyToMany between User & Role

--- src/main/java/com/example/projet_pid_2025/model/Role.java
+++ src/main/java/com/example/projet_pid_2025/model/Role.java
@@ -1,12 +1,12 @@
 package com.example.projet_pid_2025.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
 
 @Getter
 @Setter
@@ -15,6 +15,35 @@ import lombok.Setter;
 @Entity
 @Table(name = "roles")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String role;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private List<User> users;
+
+    public Role addUser(User user) {
+        if(!this.users.contains(user)) {
+            this.users.add(user);
+            user.addRole(this);
+        }
+
+        return this;
+    }
+
+    public Role removeUser(User user) {
+        if(this.users.contains(user)) {
+            this.users.remove(user);
+            user.getRoles().remove(this);
+        }
+
+        return this;
+    }
+
+
 }
